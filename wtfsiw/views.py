17a8e2468204238f6656dd788e2c4df3361748f7
Lawrence Kang <lawrencekang@gmail.com>
2014-12-09 19:59:15 -0800

    routing for auto-detect and user-input complete

--- wtfsiw/views.py
+++ wtfsiw/views.py
@@ -14,14 +14,10 @@ def index(request):
     if request.method == 'GET':
         return render(request, 'wtfsiw/index.html')
     # Invoke the Yelp API call on a POST request if location auto-detect is enabled
-    if request.method == 'POST' and request.POST.get('location-input') == '':
-        print request.POST.get('location-input')
+    if request.method == 'POST' and request.POST.get('location-input') is None:
         print 'auto-detect'
         latitude = request.POST.get('coords[latitude]')
         longitude = request.POST.get('coords[longitude]')
-        print latitude
-        print longitude
-        #position = [str(latitude), str(longitude)]
         r = requests.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=%s,%s" %(str(latitude), str(longitude)))
         if r:
             address = r.json()['results'][0]['formatted_address']
@@ -32,7 +28,7 @@ def index(request):
            return 'nothing here'
 
      # Invoke the Yelp API call on a POST request if location is user-submitted
-    if request.method == 'POST' and request.POST.get('location-input') != '':
+    if request.method == 'POST' and request.POST.get('location-input') is not None:
         submitted_loc = request.POST.get('location-input')
         print submitted_loc
         search_result = yelp.search('cafe', submitted_loc)['businesses']
@@ -41,7 +37,7 @@ def index(request):
         #return render(request, 'wtfsiw/result.html')
     
 def result(request):
-    print 'hello'
+    print 'result path'
     loc = request.POST.get('coords[latitude]')
     print loc
     return HttpResponse(loc)
