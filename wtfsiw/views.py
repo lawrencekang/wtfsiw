3b63c22e7704089ae2b5987b75a59b11a38d323a
Lawrence Kang <lawrencekang@gmail.com>
2014-12-10 12:16:03 -0800

    apis working and local storage implemented for both user-submitted and auto-detected locations

--- wtfsiw/views.py
+++ wtfsiw/views.py
@@ -16,17 +16,18 @@ def index(request):
     # Render index.html on a GET request
     if request.method == 'GET':
         return render(request, 'wtfsiw/index.html')
+
     # Flow for auto-detected location
+    # note that the user's location is passed to the browser, which then passes the address back to the server to populate the context for the Result template (to use localStorage)
     if request.method == 'POST' and request.POST.get('location-input') is None:
         latitude = request.POST.get('coords[latitude]')
         longitude = request.POST.get('coords[longitude]')
         r = requests.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=%s,%s" %(str(latitude), str(longitude)))
         if r:
             address = r.json()['results'][0]['formatted_address']
-            search_result = json.dumps(yelp.search('internet', address)['businesses'])
+            search_result = json.dumps((yelp.search('internet', address)['businesses']))
             # return the data to complete the AJAX call, and invoke a GET request for the Result page
             package = json.dumps([address, search_result])
-            print package
             return HttpResponse(package)
         else:
             return 'nothing here'
@@ -34,29 +35,29 @@ def index(request):
      # Flow for user-submitted location
     if request.method == 'POST' and request.POST.get('location-input') is not None:
         submitted_loc = request.POST.get('location-input')
-        # mark_safe used to prevent unicode and HTML conversion
         yelp_result = (yelp.search('internet', submitted_loc)['businesses'])
-        safe_result = mark_safe(json.dumps(yelp_result))
-        random_result = yelp_result[random.randrange(len(yelp_result))]
-        print random_result.get('location')['address']
-        template_data = {
-          'name': random_result.get('name'),
-          'yelp_results': safe_result,
-          'user_address': submitted_loc,
-          'destination_address': random_result.get('location')['address'][0],
-          'city': random_result.get('location')['city']
-        }
+        if yelp_result: 
+            # mark_safe used to prevent unicode and HTML conversion
+            safe_result = mark_safe(json.dumps(yelp_result))
+            random_result = yelp_result[random.randrange(len(yelp_result))]
+            template_data = {
+                'name': random_result.get('name'),
+                'yelp_results': safe_result,
+                'user_address': submitted_loc,
+                'destination_address': random_result.get('location')['address'][0],
+                'city': random_result.get('location')['city']
+            }
 
-        return render(request, 'wtfsiw/results.html', template_data)
-        #return render(request, 'wtfsiw/result.html')
-    
+            return render(request, 'wtfsiw/results.html', template_data)
+        else:
+            return HttpResponse("blah")
 def results(request):
     template_data = {
-      'name': request.GET.get('name'),
-      'destination_address': request.GET.get('location[address][]'),
-      'city': request.GET.get('location[city]')
+      'name': request.GET.get('randomLoc[name]'),
+      'destination_address': request.GET.getlist('randomLoc[location][address][]')[0],
+      'city': request.GET.get('randomLoc[location][city]'),
+      'user_address': request.GET.get('userAddress')
     }
-    print template_data
     return render(request, 'wtfsiw/results.html', template_data)
 
 def profile(request):
